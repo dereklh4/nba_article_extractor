3200226f962da5b70cacb5448504a305939bac98
Derek Hancock <derekhancock44@gmail.com>
2018-02-13 15:42:32 -0600

    initial commit

---
+++ article_extractor.py
@@ -0,0 +1,47 @@
+import urllib
+import os.path
+from bs4 import BeautifulSoup
+
+import urllib2
+starting_url = "http://www.espn.com/nba/news/archive"
+file_name = "archive.html"
+
+if not os.path.isfile(file_name):
+	response = urllib2.urlopen(starting_url)
+	html = response.read()
+	file = open(file_name,"w+")
+	file.write(html)
+	file.close()
+
+i = 0
+total = 0
+with open(file_name) as file:
+	html = file.read()
+	soup = BeautifulSoup(html)
+	li_tags = soup.find_all("li")
+	a_tags = soup.find_all("a")
+	for a_tag in a_tags:
+		if a_tag.parent.name != "li": #must be within li tag
+			continue
+		href = a_tag.get("href")
+		title = a_tag.get("title")
+		if href.find("insider") != -1: #ignore insider stories
+			continue
+		elif href.find("nba") == -1: #must be in nba
+			continue
+		elif href.find("http") == -1: #must have http link
+			continue
+		elif href.find("espn") == -1: #avoid any external links
+			continue
+		else:
+			print(href)
+			total += 1
+			i += 1
+			if i < 5:
+				response = urllib2.urlopen(href)
+				html = response.read()
+				file = open("articles/" + title,"w+")
+				file.write(html)
+				file.close()
+print("Number of articles: " + str(total))
+
---
+++ extract_raw_text.py
@@ -0,0 +1,29 @@
+from goose import Goose
+import os
+import string
+
+i = 0
+for filename in os.listdir(os.getcwd() + "/articles"):
+	f = open("articles/" + filename)
+	html = f.read()
+	#print(html)
+	f.close()
+	
+	#remove everything before 'comment' to aid goose extractor
+	idx = html.find("comment")
+	html = html[idx:]
+
+	extractor = Goose()
+	article = extractor.extract(raw_html=html)
+	text = article.cleaned_text
+	print(text)
+
+	#remove non ascii
+	printable = set(string.printable)
+	text = filter(lambda x: x in printable, text)
+
+	#save text to raw article
+	f2 = open("raw_text/" + str(i) + ". [raw] " + filename,"w+")
+	f2.write(text)
+	f2.close()
+	i += 1
